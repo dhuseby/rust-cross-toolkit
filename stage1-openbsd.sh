a001ba413c8d7633f160c16f2f92ac04e88ea2b0
Dave Huseby <dhuseby@mozilla.com>
2014-11-12 13:09:00 -0800

    changing openbsd llvm target to the user's home

--- stage1-openbsd.sh
+++ stage1-openbsd.sh
@@ -22,7 +22,7 @@ export CXXFLAGS="-I /usr/local/include/c++/4.9.0/"
 export AR="/usr/local/bin/egcc-ar"
 export NM="/usr/local/bin/egcc-nm"
 export RANLIB="/usr/local/bin/egcc-ranlib"
-LLVM_TARGET="/usr/local/"
+LLVM_TARGET=${HOME}
 
 mkdir -p ${TARGET}
 
