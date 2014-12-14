d80a1b5a745c5c81afcd54bd7060e526048ad4ca
Dave Huseby <dhuseby@mozilla.com>
2014-12-14 11:40:01 -0800

    updating stage1-linux.sh

--- stage1-linux.sh
+++ stage1-linux.sh
@@ -13,9 +13,9 @@ TOP=`pwd`
 
 export CC="/usr/bin/clang"
 export CXX="/usr/bin/clang++"
-export CFLAGS="`llvm-config --cflags`"
-export CXXFLAGS="-std=c++11 -stdlib=libc++ -stdlibabi=libc++abi `llvm-config --cxxflags`"
-export LDFLAGS="`llvm-config --ldflags`"
+export CFLAGS="-I/usr/lib/llvm-3.4/include -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -g -O2 -fomit-frame-pointer -fPIC"
+export CXXFLAGS="-std=c++11 -stdlib=libc++ -mstackrealign -g -I/usr/include/c++/v1/ -I/usr/include/libcxxabi -I/usr/lib/llvm-3.4/include  -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -g -O2 -fomit-frame-pointer -fvisibility-inlines-hidden -fno-exceptions -fPIC -Woverloaded-virtual -Wcast-qual -v"
+export LDFLAGS="-stdlib=libc++ -L/usr/lib/llvm-3.4/lib -L/usr/lib/x86_64-linux-gnu/ -L/lib64 -L/lib -L/usr/lib -lc++ -lc++abi -lunwind -lc -lpthread -lffi -ltinfo -ldl -lm"
 
 if [ ! -e rust ]; then
   git clone https://github.com/dhuseby/rust.git
