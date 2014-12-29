b1c71844b8c919de24c20db603435a5f70fce1ff
Dave Huseby <dhuseby@mozilla.com>
2014-12-29 11:03:05 -0800

    more cleanup

---
+++ stage2.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+set -x
+
+OS=`uname -s`
+
+check(){
+  if [ ${OS} != "Linux" ]; then
+    echo "You have to run this on Linux!"
+    exit 1
+  fi
+
+  if [ ! -e "stage1" ]; then
+    echo "stage1 does not exist!"
+    exit 1
+  fi
+
+  if [ ! -e "stage1/install" ]; then
+    echo "stage1/install does not exist!"
+    exit 1
+  fi
+
+  if [ ! -e "stage1/libs" ]; then
+    echo "need stage1/libs from Bitrig!"
+    exit 1
+  fi
+}
+
+setup(){
+  echo "Creating stage2"
+  mkdir -p stage2
+  cd stage2
+  TOP=`pwd`
+  RUST_PREFIX=${TOP}/../stage1/install
+  RUST_SRC=${TOP}/rust
+  RUSTC=${RUST_PREFIX}/bin/rustc
+  TARGET=x86_64-unknown-bitrig
+  DF_LIB_DIR=${TOP}/../stage1/libs
+  RS_LIB_DIR=${TOP}/rust-libs
+}
+
+clone(){
+  if [ ! -e rust ]; then
+    # clone everything
+    cd ${TOP}
+    git clone --reference ${TOP}/../stage1/rust https://github.com/rust-lang/rust.git
+    cd rust
+    git submodule init
+    git submodule update
+  else
+    # update everything
+    cd ${TOP}/rust
+    git pull origin
+    git submodule update --merge
+    rm -rf ${RS_LIB_DIR}/*
+  fi
+  cp ${TOP}/../stage1/llvmdeps.rs ${TOP}/rust/src/librustc_llvm/
+}
+
+patch_src(){
+  echo "Patching ${TOP}/${1} with ${2}.patch"
+  cd ${TOP}/${1}
+  if [ ! -e .patched ]; then
+    patch -p1 < ${TOP}/../patches/${2}.patch
+    date > .patched
+  else
+    echo "Rust already patched on:" `cat .patched`
+  fi
+}
+
+linux_build(){
+  export CC="/usr/bin/clang"
+  export CXX="/usr/bin/clang++"
+  export CFLAGS="-I/usr/lib/llvm-3.4/include -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -g -O0 -fomit-frame-pointer -fPIC"
+  export CXXFLAGS="-std=c++11 -stdlib=libc++ -mstackrealign -I/usr/include/c++/v1/ -I/usr/include/libcxxabi -I/usr/lib/llvm-3.4/include -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -g -O0 -fomit-frame-pointer -fvisibility-inlines-hidden -fno-exceptions -fPIC -Woverloaded-virtual -Wcast-qual -v"
+  export LDFLAGS="-g -stdlib=libc++ -L/usr/lib/llvm-3.4/lib -L/usr/lib/x86_64-linux-gnu/ -L/lib64 -L/lib -L/usr/lib -lc++ -lc++abi -lunwind -lc -lpthread -lffi -ltinfo -ldl -lm"
+  export CFG_VERSION="0.13.0-dev"
+  export CFG_RELEASE="bitrig-cross"
+  export CFG_VER_HASH="hash"
+  export CFG_VER_DATE="`date`"
+  export CFG_COMPILER_HOST_TRIPLE="x86_64-unknown-bitrig"
+  export CFG_PREFIX="${TOP}/../stage1/install"
+  export CFG_LLVM_LINKAGE_FILE="${TOP}/rust/src/librustc_llvm/llvmdeps.rs"
+  export RUST_FLAGS="-g"
+  RUST_LIBS="core libc alloc unicode collections rustrt rand std arena regex log fmt_macros serialize term syntax flate time getopts regex test coretest graphviz rustc_back rustc_llvm rbml rustc regex_macros green rustc_trans rustc_typeck rustc_driver rustdoc "
+
+  # compile rust libraries
+  for lib in $RUST_LIBS; do
+    if [ -e ${RS_LIB_DIR}/lib${lib}.rlib ]; then
+      echo "skipping $lib"
+    else
+      echo "compiling $lib"
+      ${RUSTC} --target ${TARGET} ${RUST_FLAGS} --crate-type lib -L${DF_LIB_DIR} -L${DF_LIB_DIR}/llvm -L${RS_LIB_DIR} ${RUST_SRC}/src/lib${lib}/lib.rs -o ${RS_LIB_DIR}/lib${lib}.rlib
+    fi
+  done
+
+  ${RUSTC} ${RUST_FLAGS} --emit obj -o ${TOP}/driver.o --target ${TARGET} -L${DF_LIB_DIR} -L${RS_LIB_DIR} --cfg rustc ${RUST_SRC}/src/driver/driver.rs
+
+  cd ${TOP}/..
+  tar cvzf stage2.tgz stage2/*.o stage2/rust-libs
+
+  echo "Please copy stage2.tgz onto your Bitrig machine and extract it"
+}
+
+linux(){
+  setup
+  clone
+  patch_src rust rust
+  patch_src rust/src/llvm llvm
+  patch_src rust/src/jemalloc jemalloc
+  linux_build
+}
+
+check
+MAKE=make
+linux
+
---
+++ stage3.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -x
+
+OS=`uname -s`
+
+check(){
+  if [ ${OS} != "Bitrig" ]; then
+    echo "You have to run this on Bitrig!"
+    exit 1
+  fi
+
+  if [ ! -e "stage1/libs" ]; then
+    echo "stage1/libs does not exist!"
+    exit 1
+  fi
+
+  if [ ! -e "stage2/rust-libs" ]; then
+    echo "stage2/rust-libs does not exist!"
+    exit 1
+  fi
+}
+
+setup(){
+  echo "Creating stage3"
+  mkdir -p stage3
+  cd stage3
+  TOP=`pwd`
+  mkdir -p bin
+  mkdir -p lib
+  RL=stage2/rust-libs
+}
+
+bitrig_build(){
+  cd ${TOP}
+  RL=${TOP}/../stage2/rust-libs
+  SUP_LIBS="-Wl,-whole-archive -lmorestack -Wl,-no-whole-archive -lrust_builtin -lrustllvm -lcompiler-rt -lbacktrace -lhoedown -lminiz -lrustrt_native"
+  LLVM_LIBS="`llvm-config --libs` -lz -lcurses"
+  RUST_DEPS="$RL/librustc.rlib $RL/libtime.rlib $RL/librustc_llvm.rlib $RL/libarena.rlib $RL/libgetopts.rlib $RL/librustc_back.rlib $RL/libsyntax.rlib $RL/libserialize.rlib $RL/librbml.rlib $RL/libflate.rlib $RL/libterm.rlib $RL/liblog.rlib $RL/libgraphviz.rlib $RL/libfmt_macros.rlib $RL/libstd.rlib $RL/librustrt.rlib $RL/libcollections.rlib $RL/libunicode.rlib $RL/liballoc.rlib $RL/liblibc.rlib $RL/librand.rlib $RL/libcore.rlib $RL/libcoretest.rlib $RL/libregex.rlib $RL/libregex_macros.rlib $RL/librustc_driver.rlib $RL/librustc_trans.rlib $RL/librustc_typeck.rlib $RL/librustdoc.rlib $RL/libtest.rlib"
+  export CXX="/usr/bin/clang++"
+  export CXXFLAGS="-I/usr/local/include  -D_DEBUG -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -g -O0 -fomit-frame-pointer -std=c++11 -stdlib=libc++ -fvisibility-inlines-hidden -fno-exceptions -fno-rtti -fPIC -ffunction-sections -fdata-sections -Wcast-qual"
+  export LDFLAGS="-L/usr/local/lib -stdlib=libc++ -lc++ -lc++abi -lm -lc -lz -lcurses -lpthread"
+
+  ${CXX} ${CXXFLAGS} -o ${TOP}/bin/rustc -Wl,--start-group ${TOP}/../stage2/driver.o ${RUST_DEPS} -L${TOP}/../stage1/libs/llvm -L${TOP}/../stage1/libs ${SUP_LIBS} ${LLVM_LIBS} -Wl,--end-group
+
+  cp ${TOP}/../stage1/libs/libcompiler-rt.a ${TOP}/lib
+  cp ${TOP}/../stage1/libs/libmorestack.a ${TOP}/lib
+  cp ${TOP}/../stage2/rust-libs/*.rlib ${TOP}/lib
+}
+
+bitrig_test(){
+  cd ${TOP}
+  ${TOP}/bin/rustc -L${TOP}/lib ${TOP}/../hw.rs
+  ./hw
+}
+
+bitrig(){
+  setup
+  bitrig_build
+  bitrig_test
+}
+
+check
+bitrig
+
