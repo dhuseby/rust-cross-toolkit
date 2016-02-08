cdfcbc67ae78fc8f000e7a80197498c391a99f1e
Dave Huseby <dhuseby@mozilla.com>
2016-02-08 13:49:49 -0800

    make drive.sh cleanup first

--- drive.sh
+++ drive.sh
@@ -32,8 +32,8 @@ copy_file() {
 }
 
 setup(){
-  #rm -rf build*.log
-  #rm -rf stage1 stage2 stage3 stage4 stage1.tgz stage2.tgz stage3.tgz
+  rm -rf build*.log
+  rm -rf stage1 stage2 stage3 stage4 stage1.tgz stage2.tgz stage3.tgz
   TOP=`pwd`
 }
 
